88f00aa1cd4b65830e602127ded7a26540da975e
vigneshiyer <vignesh.iyer@gmail.com>
2015-03-07 10:31:31 -0700

    Added the basic UI for book reading and a sample pdf file

--- TextHelperWebsite/Default.aspx
+++ TextHelperWebsite/Default.aspx
@@ -4,13 +4,30 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>EZReader</title>
 </head>
-<body>
+<body style="font-family:Tahoma;text-align:center">
     <form id="form1" runat="server">
-    <div>
-    </div>
-        <div></div>
-    </form>
+    <table align="center">
+        <tr>
+            <td>
+     <!-- This div displays the content of the document -->
+    <div id="result" runat="server" style="text-align:center;border-color:black;border-style:outset;border-width:thin;height:680px;vertical-align:central">
+    </div> <br />
+    <asp:Panel runat="server" ID="pnlShowPage" Visible="false" HorizontalAlign="Center">
+    <asp:Button ID="btnFirst" runat="server" Text="<<- First" OnClick="btnFirst_Click" /> &nbsp;
+    <asp:Button ID="btnPrev" runat="server" Text="<- Prev" OnClick="btnPrev_Click" /> &nbsp;
+    <asp:Label ID="lblShow" runat="server" Text="Showing page "></asp:Label>
+    <asp:Label ID="lblCurrentPage" runat="server" Text=""></asp:Label>
+    <asp:Label ID="lblOf" runat="server" Text=" of "></asp:Label>
+    <asp:Label ID="lblTotalPages" runat="server" Text=""></asp:Label> &nbsp;
+    <asp:Button ID="btnNext" runat="server" Text="Next ->" OnClick="btnNext_Click" /> &nbsp;
+    <asp:Button ID="btnLast" runat="server" Text="Last -->" OnClick="btnLast_Click" />
+    </asp:Panel>
+    
+    </td>
+    </tr>
+    </table>
+        </form>
 </body>
 </html>
--- TextHelperWebsite/Default.aspx.cs
+++ TextHelperWebsite/Default.aspx.cs
@@ -4,11 +4,127 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using iTextSharp.text.pdf.parser;
+using iTextSharp.text.pdf;
 
 public partial class _Default : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
+    //int noOfPages = 0;
+    int currentPage = 1;
+    string data = string.Empty;
+    PdfReader reader;
 
+    protected void Page_Load(object sender, EventArgs e)
+    {        
+        if (!IsPostBack)
+        {
+            //load data from pdf file.
+            reader = new PdfReader(Server.MapPath("SamplePDFs/Oliver-Twist.pdf"));
+            Session["document"] = reader;
+            Session["noOfPages"] = reader.NumberOfPages.ToString();
+            SetContent();
+            pnlShowPage.Visible = true;
+        }
+    }
+
+    private void SetContent()
+    {
+        reader = (PdfReader)Session["document"];
+        data = PdfTextExtractor.GetTextFromPage(reader, currentPage, new LocationTextExtractionStrategy());
+        lblTotalPages.Text = Session["noOfPages"].ToString();
+        
+        result.InnerText = "";
+        data = data.Replace("\n", "<br>");
+        string[] keywords = { "Application", "verify", "Software" };
+        lblCurrentPage.Text = currentPage.ToString();     
+   
+        bool isKeyWordPresent = false;
+        foreach (string item in keywords)
+        {
+            if (data.Contains(item))
+            {
+                isKeyWordPresent = true;
+                break;
+            }
+        }
+        if (isKeyWordPresent)
+        {
+            string[] text = data.Split(' ');
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (keywords.Contains(text[i].Replace("<br>", "")))
+                {
+                    result.InnerHtml += "<a href=\"#\">" + text[i] + "</a> ";
+                }
+                else
+                {
+                    result.InnerHtml += text[i] + " ";
+                }
+            }
+
+        }
+        else
+        {
+            result.InnerHtml = data;
+        }
+
+        if (currentPage < Convert.ToInt32(Session["noOfPages"].ToString()))
+            btnNext.Enabled = true;
+        else
+            btnNext.Enabled = false;
+
+        if (currentPage > 1)
+            btnPrev.Enabled = true;
+        else
+            btnPrev.Enabled = false;
+    }
+    protected void btnNext_Click(object sender, EventArgs e)
+    {
+        if (Convert.ToInt32(lblCurrentPage.Text) < Convert.ToInt32(lblTotalPages.Text))
+        {
+            btnNext.Enabled = true;
+            currentPage = Convert.ToInt32(lblCurrentPage.Text) + 1;
+            //if (currentPage == Convert.ToInt32(Session["noOfPages"].ToString()))
+            //    btnNext.Enabled = false;
+            //else
+            //    btnNext.Enabled = true;
+            SetContent();
+        }
+        
+        else
+        {
+            btnNext.Enabled = false;
+        }
+    }
+    protected void btnPrev_Click(object sender, EventArgs e)
+    {
+        if (Convert.ToInt32(lblCurrentPage.Text) >= 1)
+        {
+            btnPrev.Enabled = true;
+            btnNext.Enabled = true;
+            currentPage = Convert.ToInt32(lblCurrentPage.Text) - 1;
+            //if (currentPage == 1)
+            //    btnPrev.Enabled = false;
+            //else
+            //    btnPrev.Enabled = true;
+            SetContent();
+        }
+        else
+        {
+            btnPrev.Enabled = false;
+        }
+    }
+    protected void btnFirst_Click(object sender, EventArgs e)
+    {
+        currentPage = 1;
+        SetContent();
+        //btnPrev.Enabled = false;
+    }
+    protected void btnLast_Click(object sender, EventArgs e)
+    {
+        lblTotalPages.Text = Session["noOfPages"].ToString();
+        currentPage = Convert.ToInt32(Session["noOfPages"].ToString());
+        SetContent();
+        //btnNext.Enabled = false;
     }
 }
